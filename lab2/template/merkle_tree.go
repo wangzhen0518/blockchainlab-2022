f57d6ec168deb72984b404418fbe9283e053ad0b
汪震 <wangzhen0518@126.com>
2022-04-23 15:30:20 +0800

    merge lab1

--- lab2/template/merkle_tree.go
+++ lab2/template/merkle_tree.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"bytes"
+	"math"
+)
+
 // MerkleTree represent a Merkle tree
 type MerkleTree struct {
 	RootNode *MerkleNode
@@ -15,16 +20,43 @@ type MerkleNode struct {
 // NewMerkleTree creates a new Merkle tree from a sequence of data
 // implement
 func NewMerkleTree(data [][]byte) *MerkleTree {
-	var node = MerkleNode{nil, nil, data[0]}
-	var mTree = MerkleTree{&node}
+	num := len(data)
+	depth := math.Ceil(math.Log2(float64(num)))
+	node, _ := dfs(data, int(depth), 0, num)
+	var mTree = MerkleTree{node}
 
 	return &mTree
+}
+
+func dfs(data [][]byte, depth, num, numLimit int) (*MerkleNode, int) {
+	if depth == 0 {
+		if num < numLimit {
+			return NewMerkleNode(nil, nil, data[num]), num + 1
+		}
+		return nil, num
+	}
+	left, numLeft := dfs(data, depth-1, num, numLimit)
+	right, numRight := dfs(data, depth-1, numLeft, numLimit)
+	node, numTol := NewMerkleNode(left, right, nil), numRight
+	return node, numTol
 }
 
 // NewMerkleNode creates a new Merkle tree node
 // implement
 func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
 	node := MerkleNode{}
-
+	node.Left = left
+	node.Right = right
+	temp := [32]byte{}
+	if left == nil {
+		temp = mySha256(data)
+	} else {
+		if right == nil { //奇数情况
+			right = &MerkleNode{nil, nil, left.Data}
+		}
+		temp = mySha256(bytes.Join([][]byte{left.Data, right.Data}, []byte{}))
+	}
+	node.Data = make([]byte, 32)
+	copy(node.Data, temp[:])
 	return &node
 }
--- lab2/template/sha256.go
+++ lab2/template/sha256.go
@@ -1,15 +1,14 @@
 package main
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 func mySha256(message []byte) [32]byte {
 	//前八个素数平方根的小数部分的前面32位
-	h0 := uint32(0x6a09e667)
-	h1 := uint32(0xbb67ae85)
-	h2 := uint32(0x3c6ef372)
-	h3 := uint32(0xa54ff53a)
-	h4 := uint32(0x510e527f)
-	h5 := uint32(0x9b05688c)
-	h6 := uint32(0x1f83d9ab)
-	h7 := uint32(0x5be0cd19)
+	h := [8]uint32{
+		0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19}
 
 	//自然数中前面64个素数的立方根的小数部分的前32位
 	k := [64]uint32{
@@ -22,6 +21,99 @@ func mySha256(message []byte) [32]byte {
 		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
 		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2}
 
+	tolMessage, N := fillMessage(message)
+	N /= 64
+	div512Message := [][]byte{}
+	for i := 0; i < N; i++ {
+		div512Message = append(div512Message, tolMessage[i*64:i*64+64])
+	}
+
+	for i := 0; i < N; i++ {
+		div32Message := seperate32Message(div512Message[i])
+
+		loopVar := h
+		// a-0, b-1, c-2, d-3, e-4, f-5, g-6, h-7
+		for p := 0; p < 64; p++ {
+			t1 := loopVar[7] + _SIGMA1(loopVar[4]) + _Ch(loopVar[4], loopVar[5], loopVar[6]) + k[p] + div32Message[p]
+			t2 := _SIGMA0(loopVar[0]) + _Maj(loopVar[0], loopVar[1], loopVar[2])
+			for q := 7; q > 0; q-- {
+				loopVar[q] = loopVar[q-1]
+			}
+			loopVar[4] += t1
+			loopVar[0] = t1 + t2
+		}
+		for p := 0; p < 8; p++ {
+			h[p] += loopVar[p]
+		}
+	}
+
+	tempDate := []byte{}
 	sha256data := [32]byte{}
+	for i := 0; i < 8; i++ {
+		tempDate = append(tempDate, uint32ToBytes(h[i])...)
+	}
+	copy(sha256data[:], tempDate)
 	return sha256data
+}
+
+func int64ToBytes(i int64) []byte {
+	var buf = make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, uint64(i))
+	return buf
+}
+func uint32ToBytes(i uint32) []byte {
+	var buf = make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, i)
+	return buf
+}
+
+func fillMessage(message []byte) (plus_message []byte, num8 int) {
+	by0 := []byte{0b00000000}
+	by1 := []byte{0b10000000}
+	// 需要补k个0，满足 8*length + 1 + k = 512m + 448
+	// 从而 k + 1 + 8*length mod 512 =448
+	// 即 k = 448 - 8*length mod 512 - 1
+	// 所以8位的1Byte 0有 (448 - 8*length mod 512 - 1)/8 个
+	// 额外有一列 '1' + '0'*(63 - length mod 64) mod 8
+	length := len(message)
+	k := (448 - (8*length)%512 - 1) / 8
+	app := bytes.Repeat(by0, k)
+	temp := int64ToBytes(int64(8 * length))
+	plus_message = bytes.Join([][]byte{message, by1, app, temp}, []byte{})
+	num8 = len(plus_message)
+	return
+}
+
+// seperate32Message 将数据分为32位一组的包，使用uint32是为了后续的位运算，否则继续使用byte，位运算结果将出错
+func seperate32Message(div512Message []byte) (div32Message []uint32) {
+	for i := 0; i < 16; i++ {
+		div32Message = append(div32Message, binary.BigEndian.Uint32(div512Message[i*4:i*4+4]))
+	}
+	for i := 16; i < 64; i++ {
+		div32Message = append(div32Message, _sigma1(div32Message[i-2])+div32Message[i-7]+
+			_sigma0(div32Message[i-15])+div32Message[i-16])
+	}
+	return
+}
+
+func rightRotate(n uint32, d uint) uint32 {
+	return ((n >> d) | (n << (32 - d))) & ((1 << 32) - 1)
+}
+func _Ch(x, y, z uint32) uint32 {
+	return (x & y) ^ ((^x) & z)
+}
+func _Maj(x, y, z uint32) uint32 {
+	return (x & y) ^ (x & z) ^ (y & z)
+}
+func _SIGMA0(n uint32) uint32 {
+	return rightRotate(n, 2) ^ rightRotate(n, 13) ^ rightRotate(n, 22)
+}
+func _SIGMA1(n uint32) uint32 {
+	return rightRotate(n, 6) ^ rightRotate(n, 11) ^ rightRotate(n, 25)
+}
+func _sigma0(n uint32) uint32 {
+	return rightRotate(n, 7) ^ rightRotate(n, 18) ^ (n >> 3)
+}
+func _sigma1(n uint32) uint32 {
+	return rightRotate(n, 17) ^ rightRotate(n, 19) ^ (n >> 10)
 }
